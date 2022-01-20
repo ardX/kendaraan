c317df96efb96185ba8193592937f9810a1cf429
ardX <ardianyusufw@gmail.com>
2022-01-21 05:31:57 +0700

    create model Kendaraan

---
+++ app/Http/Controllers/KendaraanController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Kendaraan;
+use Illuminate\Http\Request;
+
+class KendaraanController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Kendaraan  $kendaraan
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Kendaraan $kendaraan)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Kendaraan  $kendaraan
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Kendaraan $kendaraan)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Kendaraan  $kendaraan
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Kendaraan $kendaraan)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Kendaraan  $kendaraan
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Kendaraan $kendaraan)
+    {
+        //
+    }
+}
