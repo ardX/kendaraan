8e2931552cb9f176f09c0791983f3abba0bbd7ba
ardX <ardianyusufw@gmail.com>
2022-01-21 05:45:43 +0700

    update to jessegers and add fillable to Kendaraan

--- app/Models/Kendaraan.php
+++ app/Models/Kendaraan.php
@@ -2,10 +2,27 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model;
 
 class Kendaraan extends Model
 {
-    use HasFactory;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'title',
+        'description'
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'created_at',
+        'updated_at'
+    ];
 }
