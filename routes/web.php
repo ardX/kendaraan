c17692073deeae6fcedc8e7bfb800ac10bdc5b2e
ardX <ardianyusufw@gmail.com>
2022-01-23 05:06:41 +0700

    move to api

--- routes/web.php
+++ routes/web.php
@@ -16,12 +16,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::resource('mobil', '\App\Http\Controllers\MobilController');
-Route::resource('motor', '\App\Http\Controllers\MotorController');
-Route::resource('jual/mobil', '\App\Http\Controllers\JualMobilController');
-Route::resource('jual/motor', '\App\Http\Controllers\JualMotorController');
-Route::get('stock/mobil', ['as' => 'stock.mobil', 'uses' => '\App\Http\Controllers\MobilController@stock']);
-Route::get('stock/motor', ['as' => 'stock.motor', 'uses' => '\App\Http\Controllers\MotorController@stock']);
-Route::get('report/mobil/{id?}', ['as' => 'report.mobil', 'uses' => '\App\Http\Controllers\JualMobilController@report']);
-Route::get('report/motor/{id?}', ['as' => 'report.motor', 'uses' => '\App\Http\Controllers\JualMotorController@report']);
